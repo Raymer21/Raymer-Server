4df831257ff8856fe498c8c43febd9f7e9ad5160
Jaxson R <life.is.cringy@gmail.com>
2021-09-19 19:54:23 -0500

    Demo Test Multi-1

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            float moveHorizontal = Input.GetAxisRaw("Horizontal");
+            float moveVertical = Input.GetAxisRaw("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal * 0.1f, 0, moveVertical * 0.1f);
+            transform.position = transform.position + movement;
+        }
+    }
+
+    void Update()
+    {
+        HandleMovement();
+    }
+}
